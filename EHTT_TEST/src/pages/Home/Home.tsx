50432acfadc7a077a0bc932ae73988a5aff6e87b
Andres F. Fajardo <andrezffajardo@gmail.com>
2023-01-02 22:31:45 -0500

    Table implement

--- EHTT_TEST/src/pages/Home/Home.tsx
+++ EHTT_TEST/src/pages/Home/Home.tsx
@@ -1,10 +1,33 @@
 import React from 'react';
-
+import { DataGrid, GridRenderCellParams } from '@mui/x-data-grid';
+import { People } from '@/data';
 
 export interface HomeInterface {}
 
 const Home: React.FC<HomeInterface> = () => {
-	return <div >Home</div>;
+	const pageSize = 5;
+	const columns = [
+		{
+		field: 'name',
+		headerName: 'Name',
+		flex: 1,
+		minWidth: 150,
+		renderCell: (params: GridRenderCellParams) => <>{params.field}</>
+	}
+];
+	return (
+		<div >
+			<DataGrid
+				rows={People}
+				columns={columns}
+				disableColumnSelector
+				disableSelectionOnClick
+				autoHeight
+				pageSize={pageSize}
+				rowsPerPageOptions={[pageSize]}
+			/>
+		</div>
+	);
 };
 
 export default Home;

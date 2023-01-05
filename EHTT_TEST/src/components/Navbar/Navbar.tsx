77f8a2ebc54e53ec8a73701175df1eac57e93cc1
Andres F. Fajardo <andrezffajardo@gmail.com>
2023-01-04 23:06:16 -0500

    change Navbar tittle, add styled component

--- EHTT_TEST/src/components/Navbar/Navbar.tsx
+++ EHTT_TEST/src/components/Navbar/Navbar.tsx
@@ -1,20 +1,18 @@
-import { AppBar, Toolbar, Typography } from '@mui/material';
-import React from 'react';
-
+import { AppBar, Toolbar, Typography } from "@mui/material";
+import React from "react";
 
 export interface NavbarInterface {}
 
 const Navbar: React.FC<NavbarInterface> = () => {
-	return (
-		<AppBar position="fixed">
-        <Toolbar>
-          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-            React Test
-          </Typography>
-        </Toolbar>
-      </AppBar>
-	);
+  return (
+    <AppBar position="fixed">
+      <Toolbar>
+        <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+          Employee Happiness Tracking Tool
+        </Typography>
+      </Toolbar>
+    </AppBar>
+  );
 };
 
 export default Navbar;
-

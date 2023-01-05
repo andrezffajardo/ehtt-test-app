22131639045319c23a62989f7f8a2979cf886500
Andres F. Fajardo <andrezffajardo@gmail.com>
2023-01-04 21:53:28 -0500

    Add columns, quit div Home

--- EHTT_TEST/src/pages/Home/Home.tsx
+++ EHTT_TEST/src/pages/Home/Home.tsx
@@ -1,34 +1,44 @@
-import React from 'react';
-import { DataGrid, GridRenderCellParams } from '@mui/x-data-grid';
-import { People } from '@/data';
+import React from "react";
+import { DataGrid, GridRenderCellParams } from "@mui/x-data-grid";
+import { People } from "@/data";
 
 export interface HomeInterface {}
 
 const Home: React.FC<HomeInterface> = () => {
-	const pageSize = 5;
-	const columns = [
-		{
-		field: 'name',
-		headerName: 'Name',
-		flex: 1,
-		minWidth: 150,
-		renderCell: (params: GridRenderCellParams) => <>{params.field}</>
-	}
-];
-	return (
-		<div >
-			<DataGrid
-				rows={People}
-				columns={columns}
-				disableColumnSelector
-				disableSelectionOnClick
-				autoHeight
-				pageSize={pageSize}
-				rowsPerPageOptions={[pageSize]}
-			/>
-		</div>
-	);
+  const pageSize = 5;
+  const columns = [
+    {
+      field: "name",
+      headerName: "Name",
+      flex: 1,
+      minWidth: 150,
+      renderCell: (params: GridRenderCellParams) => <>{params.value}</>,
+    },
+    {
+      field: "category",
+      headerName: "Categories",
+      flex: 1,
+      renderCell: (params: GridRenderCellParams) => <>{params.value}</>,
+    },
+    {
+      field: "company",
+      headerName: "Company",
+      flex: 1,
+      renderCell: (params: GridRenderCellParams) => <>{params.value}</>,
+    },
+  ];
+  return (
+    <DataGrid
+      rows={People}
+      columns={columns}
+      disableColumnSelector
+      disableSelectionOnClick
+      autoHeight
+      pageSize={pageSize}
+      rowsPerPageOptions={[pageSize]}
+      getRowId={(row: any) => row.id}
+    />
+  );
 };
 
 export default Home;
-
